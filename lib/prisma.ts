36ab63ed5c0cfed799b1fac6bb59368c895fde01
Abhishek jaiswar <abhishekndxw@gmail.com>
2025-10-05 22:15:05 +0530

    fix: remove previous prisma generated files regenerate prisma and fix path

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,4 @@
-/* eslint-disable */
-
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@/lib/generated/prisma";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
