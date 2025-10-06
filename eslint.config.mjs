1ea42392bf2a7d2f62cf0e3412a35ab47ab44129
Abhishek jaiswar <abhishekndxw@gmail.com>
2025-10-07 02:22:29 +0530

    fix: es

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,14 +26,6 @@ const eslintConfig = [
 
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-
-      // (Optional) Add common project preferences
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-      ],
-      "react-hooks/exhaustive-deps": "warn",
-      "react/no-unescaped-entities": "off",
     },
   },
 ];
